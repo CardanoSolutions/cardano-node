b65bf739b037252c3e70df28b714b117ba7e5669
KtorZ <matthias.benkort@gmail.com>
2023-09-20 18:18:18 +0200

    Wire ledger events through the node
      Using a dumb handler for now that just prints things on stdout.
      Some notes:
    
      - Conway events are currently ignored.
    
      - The 'TICK' and 'BBODY' events still need to be mapped.
    
      - I took back the code from https://github.com/abailly-iohk/ouroboros-network/commit/3211516cdf380f4eeb99c881b480c2334b61225a
        though it was operating at slightly more abstract level. At the
        place we currently install the handler, we have a `BlockType` GADT
        which makes the `blk` type parameter very concrete, so we could
        possibly do something simpler?
    
      - This runs (:tada:), though it seems to print way more messages than
        expected; unless the block is really applying many many blocks,
        which could be a possibility too.

--- cardano-node/src/Cardano/Node/Run.hs
+++ cardano-node/src/Cardano/Node/Run.hs
@@ -78,6 +78,7 @@ import           Cardano.Node.Configuration.NodeAddress
 import           Cardano.Node.Configuration.POM (NodeConfiguration (..),
                    PartialNodeConfiguration (..), SomeNetworkP2PMode (..),
                    defaultPartialNodeConfiguration, makeNodeConfiguration, parseNodeConfigurationFP)
+import           Cardano.Node.LedgerEvent
 import           Cardano.Node.Startup
 import           Cardano.Node.Tracing.API
 import           Cardano.Node.Tracing.StateRep (NodeState (NodeKernelOnline))
@@ -87,6 +88,7 @@ import           Cardano.Tracing.Config (TraceOptions (..), TraceSelection (..))
 
 import qualified Ouroboros.Consensus.Config as Consensus
 import           Ouroboros.Consensus.Config.SupportsNode (ConfigSupportsNode (..))
+import           Ouroboros.Consensus.Ledger.Basics (AuxLedgerEvent, LedgerState)
 import           Ouroboros.Consensus.Node (NetworkP2PMode (..), RunNodeArgs (..),
                    StdRunNodeArgs (..))
 import qualified Ouroboros.Consensus.Node as Node (getChainDB, run)
@@ -164,8 +166,17 @@ runNode cmdPc = do
               in getNetworkMagic $ Consensus.configBlock pInfoConfig
 
     case p of
-      SomeConsensusProtocol _ runP ->
-        handleNodeWithTracers cmdPc nc p networkMagic runP
+      SomeConsensusProtocol blk runP ->
+        handleNodeWithTracers
+          (case blk of
+            Api.CardanoBlockType -> \event -> do
+              putStrLn $ "New ledger event: " <> show (convertAuxLedgerEvent event)
+            Api.ByronBlockType{} ->
+              const $ pure ()
+            Api.ShelleyBlockType{} ->
+              const $ pure ()
+          )
+          cmdPc nc p networkMagic runP
 
 -- | Workaround to ensure that the main thread throws an async exception on
 -- receiving a SIGTERM signal.
@@ -189,13 +200,14 @@ handleNodeWithTracers
   :: ( TraceConstraints blk
      , Api.Protocol IO blk
      )
-  => PartialNodeConfiguration
+  => (AuxLedgerEvent (LedgerState blk) -> IO ())
+  -> PartialNodeConfiguration
   -> NodeConfiguration
   -> SomeConsensusProtocol
   -> Api.NetworkMagic
   -> Api.ProtocolInfoArgs IO blk
   -> IO ()
-handleNodeWithTracers cmdPc nc p networkMagic runP = do
+handleNodeWithTracers handleLedgerEvent cmdPc nc p networkMagic runP = do
   -- This IORef contains node kernel structure which holds node kernel.
   -- Used for ledger queries and peer connection status.
   nodeKernelData <- mkNodeKernelData
@@ -219,7 +231,7 @@ handleNodeWithTracers cmdPc nc p networkMagic runP = do
           mapM_ (traceWith $ startupTracer tracers) startupInfo
           traceNodeStartupInfo (nodeStartupInfoTracer tracers) startupInfo
 
-          handleSimpleNode runP p2pMode tracers nc
+          handleSimpleNode handleLedgerEvent runP p2pMode tracers nc
             (\nk -> do
                 setNodeKernel nodeKernelData nk
                 traceWith (nodeStateTracer tracers) NodeKernelOnline)
@@ -258,7 +270,7 @@ handleNodeWithTracers cmdPc nc p networkMagic runP = do
 
           -- We ignore peer logging thread if it dies, but it will be killed
           -- when 'handleSimpleNode' terminates.
-          handleSimpleNode runP p2pMode tracers nc
+          handleSimpleNode handleLedgerEvent runP p2pMode tracers nc
             (\nk -> do
                 setNodeKernel nodeKernelData nk
                 traceWith (nodeStateTracer tracers) NodeKernelOnline)
@@ -325,7 +337,8 @@ handlePeersListSimple tr nodeKern = forever $ do
 
 handleSimpleNode
   :: forall blk p2p . Api.Protocol IO blk
-  => Api.ProtocolInfoArgs IO blk
+  => (AuxLedgerEvent (LedgerState blk) -> IO ())
+  -> Api.ProtocolInfoArgs IO blk
   -> NetworkP2PMode p2p
   -> Tracers RemoteConnectionId LocalConnectionId blk p2p
   -> NodeConfiguration
@@ -334,7 +347,7 @@ handleSimpleNode
   -- layer is initialised.  This implies this function must not block,
   -- otherwise the node won't actually start.
   -> IO ()
-handleSimpleNode runP p2pMode tracers nc onKernel = do
+handleSimpleNode handleLedgerEvent runP p2pMode tracers nc onKernel = do
   logStartupWarnings
 
   traceWith (startupTracer tracers)
@@ -397,19 +410,20 @@ handleSimpleNode runP p2pMode tracers nc onKernel = do
 
   withShutdownHandling (ncShutdownConfig nc) (shutdownTracer tracers) $
     let nodeArgs = RunNodeArgs
-          { rnTraceConsensus = consensusTracers tracers
-          , rnTraceNTN       = nodeToNodeTracers tracers
-          , rnTraceNTC       = nodeToClientTracers tracers
-          , rnProtocolInfo   = pInfo
-          , rnNodeKernelHook = \registry nodeKernel -> do
+          { rnTraceConsensus    = consensusTracers tracers
+          , rnTraceNTN          = nodeToNodeTracers tracers
+          , rnTraceNTC          = nodeToClientTracers tracers
+          , rnProtocolInfo      = pInfo
+          , rnEnableP2P         = p2pMode
+          , rnPeerSharing       = ncPeerSharing nc
+          , rnHandleLedgerEvent = handleLedgerEvent
+          , rnNodeKernelHook    = \registry nodeKernel -> do
               maybeSpawnOnSlotSyncedShutdownHandler
                 (ncShutdownConfig nc)
                 (shutdownTracer tracers)
                 registry
                 (Node.getChainDB nodeKernel)
               onKernel nodeKernel
-          , rnEnableP2P      = p2pMode
-          , rnPeerSharing    = ncPeerSharing nc
           }
     in case p2pMode of
       EnabledP2PMode -> do
