29ae427f918d5372b985bd32f1142d86a7e6bd0a
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-09-21 16:06:50 +0200

    Add utility to decode ledger events file

--- cardano-node/src/Cardano/Node/Run.hs
+++ cardano-node/src/Cardano/Node/Run.hs
@@ -185,11 +185,13 @@ runNode cmdPc = do
           handleNodeWithTracers
             (case blockType of
               Api.CardanoBlockType -> LedgerEventHandler $ \headerHash slotNo event -> do
-                  BS.hPut h $ serialize' (eventCodecVersion event)
-                    ( getOneEraHash headerHash
-                    , slotNo
-                    , fromAuxLedgerEvent event
-                    )
+                let codecVersion = eventCodecVersion event
+                    bytes = serialize' codecVersion
+                      ( getOneEraHash headerHash
+                      , slotNo
+                      , fromAuxLedgerEvent event
+                      )
+                BS.hPut h $ (serialize' codecVersion $ BS.length bytes) <> bytes
               Api.ByronBlockType{} ->
                 discardEvent
               Api.ShelleyBlockType{} ->
