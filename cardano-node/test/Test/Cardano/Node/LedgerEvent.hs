f74d7d46ef42fa324dfb46f7b18344f964039f39
KtorZ <matthias.benkort@gmail.com>
2023-09-26 15:49:08 +0200

    Define new property for validating CDDL conformance of ledger events.
      Also include a first (near empty) draft of a CDDL specification.

--- cardano-node/test/Test/Cardano/Node/LedgerEvent.hs
+++ cardano-node/test/Test/Cardano/Node/LedgerEvent.hs
@@ -3,13 +3,20 @@
 module Test.Cardano.Node.LedgerEvent where
 
 import           Cardano.Node.LedgerEvent
+import qualified Codec.CBOR.Schema as CDDL
 import           Data.ByteString.Short(toShort)
 import           Data.ByteString.Lazy(fromStrict)
 import qualified Data.ByteString.Base16 as Hex
-import           Hedgehog (Property, discover, footnote)
+import qualified Data.Text.IO as TIO
+import           Hedgehog (Property, discover, footnote, (===))
 import qualified Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
+import           System.IO.Unsafe(unsafePerformIO)
+
+specification :: Text
+specification =
+  unsafePerformIO $ TIO.readFile "ledger_event.cddl"
 
 prop_roundtrip_LedgerEvent_CBOR :: Property
 prop_roundtrip_LedgerEvent_CBOR =
@@ -18,6 +25,14 @@ prop_roundtrip_LedgerEvent_CBOR =
     event <- Hedgehog.forAll genEvent
     footnote ("serialized event: " <> show (Hex.encode $ serializeEvent version event))
     Hedgehog.tripping event (serializeEvent version) (deserializeEvent . fromStrict)
+
+prop_cddl_validation_LedgerEvent :: Property
+prop_cddl_validation_LedgerEvent =
+  Hedgehog.property $ do
+    version <- Hedgehog.forAll Gen.enumBounded
+    event <- Hedgehog.forAll genEvent
+    let bytes = serializeEvent version event
+    CDDL.validate specification bytes === Right ()
 
 genEvent :: Hedgehog.Gen AnchoredEvent
 genEvent =
