38c0b6cf36c65fd5017300e3fa46e3b9a59dc0e9
KtorZ <matthias.benkort@gmail.com>
2023-09-28 13:36:10 +0200

    Return errors as string from validate_cbor
      So that we don't panic in the function and we keep everything pure. Now we can properly hook into Hedgehog annotation system to provide better failures.

--- cddl/cbits/libcddl.h
+++ cddl/cbits/libcddl.h
@@ -6,9 +6,9 @@
 
 extern "C" {
 
-void validate_cbor(const uint8_t *cddl_ptr,
-                   uintptr_t cddl_len,
-                   const uint8_t *cbor_ptr,
-                   uintptr_t cbor_len);
+int8_t *validate_cbor(const uint8_t *cddl_ptr,
+                      uintptr_t cddl_len,
+                      const uint8_t *cbor_ptr,
+                      uintptr_t cbor_len);
 
 } // extern "C"
