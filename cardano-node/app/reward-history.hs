b1e5cd71cab7ad7cf7a1cb03611ca6da8db6606d
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-10-01 08:58:54 +0200

    Remove unneeded import

--- cardano-node/app/reward-history.hs
+++ cardano-node/app/reward-history.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE LambdaCase #-}
 
-import Cardano.Node.LedgerEvent (parseStakeCredential, foldEvent, filterRewards, AnchoredEvent (AnchoredEvent), LedgerEvent (LedgerNewEpochEvent), LedgerNewEpochEvent(LedgerStakeDistEvent))
-import System.Environment (getArgs)
-import System.IO (IOMode(ReadMode))
-import Network.Socket
+import Cardano.Node.LedgerEvent (
+    AnchoredEvent (AnchoredEvent),
+    LedgerEvent (LedgerNewEpochEvent),
+    LedgerNewEpochEvent (LedgerStakeDistEvent),
+    foldEvent,
+    parseStakeCredential,
+ )
 import Control.Exception (bracket, bracketOnError)
+import Network.Socket
+import System.Environment (getArgs)
+import System.IO (IOMode (ReadMode))
 
 -- Usage: reward-history <<<stdin LEDGER-EVENTS] <STAKE-ADDRESS>
 --
@@ -13,21 +19,20 @@ import Control.Exception (bracket, bracketOnError)
 --   cat ledger_events.cbor | rewards-history "e04cf4f01890215bd181d1fcd3c9589a2a4a3adbcff1a70b748080fa82"
 main :: IO ()
 main = do
-  stakeCredential <- getArgs >>= expectStakeCredential . head
-  print $ "Got stake credential: " ++ show stakeCredential
+    stakeCredential <- getArgs >>= expectStakeCredential . head
+    print $ "Got stake credential: " ++ show stakeCredential
 
-  runTCPClient "localhost" "9999" $ \sock -> do
-    h <- socketToHandle sock ReadMode
+    runTCPClient "localhost" "9999" $ \sock -> do
+        h <- socketToHandle sock ReadMode
 
-    putStrLn "Getting reward history..."
-    -- void $ foldEvent h mempty $ \st e -> let r = filterRewards stakeCredential st e in print r >> pure r
-    -- void $ foldEvent h mempty $ \st e -> let r = filterRewards stakeCredential st e in print r >> pure r
-    foldEvent h () $ \() -> \case AnchoredEvent _ _ (LedgerNewEpochEvent (LedgerStakeDistEvent e)) -> print e; _otherEvent -> pure ()
- where
-  expectStakeCredential =
-    maybe (error "invalid / missing stake address as 1st argument") return
-    .
-    parseStakeCredential
+        putStrLn "Getting reward history..."
+        -- void $ foldEvent h mempty $ \st e -> let r = filterRewards stakeCredential st e in print r >> pure r
+        -- void $ foldEvent h mempty $ \st e -> let r = filterRewards stakeCredential st e in print r >> pure r
+        foldEvent h () $ \() -> \case AnchoredEvent _ _ (LedgerNewEpochEvent (LedgerStakeDistEvent e)) -> print e; _otherEvent -> pure ()
+  where
+    expectStakeCredential =
+        maybe (error "invalid / missing stake address as 1st argument") return
+            . parseStakeCredential
 
 runTCPClient :: HostName -> ServiceName -> (Socket -> IO a) -> IO a
 runTCPClient host port client = withSocketsDo $ do
@@ -36,7 +41,7 @@ runTCPClient host port client = withSocketsDo $ do
     bracket (open addrInfo) close client
   where
     resolve = do
-        let hints = defaultHints { addrSocketType = Stream, addrFamily = AF_INET }
+        let hints = defaultHints{addrSocketType = Stream, addrFamily = AF_INET}
         head <$> getAddrInfo (Just hints) (Just host) (Just port)
     open addr = bracketOnError (openSocket addr) close $ \sock -> do
         connect sock $ addrAddress addr
